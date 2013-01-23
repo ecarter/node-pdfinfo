2e827a04d365264d92bdca2ecc97d449f51837a7
Erin Carter <me@erincarter.com>
2013-01-23 17:13:13 -0600

    added pages test

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ var PDF = require('..');
 
 describe('#PDF', function(){
 
-  it('should return pdf title', function(done){
+  it('should return title', function(done){
     var pdf = PDF(__dirname + '/pdf/basic.pdf');
 
     pdf.exec(function(err, meta){
@@ -11,6 +11,26 @@ describe('#PDF', function(){
       meta.title.should.equal('basic');
       done();
     })
-  });
+  })
+
+  it('should return pages', function(done){
+    var pdf = PDF(__dirname + '/pdf/pages.pdf');
+
+    pdf.exec(function(err, meta){
+      if (err) return done(err);
+      meta.pages.should.equal(6);
+      done();
+    })
+  })
+
+  it('should return tagged', function(done){
+    var pdf = PDF(__dirname + '/pdf/pages.pdf');
+
+    pdf.exec(function(err, meta){
+      if (err) return done(err);
+      meta.tagged.should.equal(false);
+      done();
+    })
+  })
 
 })
